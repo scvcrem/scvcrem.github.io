cd7c5dc343284dfb3e7a0174e404abc3f4e5749d
scvcrem <simone.cremaschi@unibocconi.it>
2026-04-29 14:25:56 +0100

    add media mention by ilmanifesto

---
+++ _bibliography/auto/papers.el
@@ -0,0 +1,20 @@
+;; -*- lexical-binding: t; -*-
+
+(TeX-add-style-hook
+ "papers"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "nhs_backlash"
+    "cremaschi2025likeacctv"
+    "xyella"
+    "vaia"
+    "public_services"
+    "legacies_resistance"
+    "cremaschiDevillanova2020"
+    "cremaschietal2016"
+    "male_backlash"
+    "cremaschi2021ciaoousmane"
+    "Cremaschi2020"
+    "cremaschi2017diversityandcontact"))
+ '(or :bibtex :latex))
+
